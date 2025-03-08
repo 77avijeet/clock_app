ca0bf3f7a9b9ff5edaeba58bfe5527bcdd898ed2
avijeet <avijeetbhuia9386@gmail.com>
2025-03-08 17:18:08 +0530

    color change

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -38,7 +38,7 @@ class HomeScreen extends StatelessWidget {
                         height: 250,
                         width: 250,
                         margin: const EdgeInsets.only(top: 50.0),
-                        color: const Color.fromARGB(255, 249, 180, 7),
+                        color: const Color.fromARGB(255, 220, 8, 174),
                       ),
                     ],
                   ),
