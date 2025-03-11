5014407e0eb2323c6e7e84d40911ef25475f9ca0
avijeet <avijeetbhuia9386@gmail.com>
2025-03-11 08:54:48 +0530

    hight and width change

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -35,8 +35,8 @@ class HomeScreen extends StatelessWidget {
                         ],
                       ),
                       Container(
-                        height: 200,
-                        width: 200,
+                        height: 250,
+                        width: 250,
                         margin: const EdgeInsets.only(top: 50.0),
                         color: const Color.fromARGB(255, 240, 234, 238),
                       ),
