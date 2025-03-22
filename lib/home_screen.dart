7e08a9bd915e909d40c192439689a7442485b8e1
avijeet <avijeetbhuia9386@gmail.com>
2025-03-22 12:57:39 +0530

    remove stack

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -10,40 +10,36 @@ class HomeScreen extends StatelessWidget {
         length: 3,
         child: Stack(
           children: [
-            Stack(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [Color.fromARGB(255, 194, 221, 18), Color(0xFFD1B964)],
-                    ),
-                  ),
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Color.fromARGB(255, 194, 221, 18), Color(0xFFD1B964)],
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 60.0),
-
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      TabBar(
-                        tabs: [
-                          Tab(text: "Time"),
-                          Tab(text: "Stopwatch"),
-                          Tab(text: "Timer"),
-                        ],
-                      ),
-                      Container(
-                        height: 250,
-                        width: 250,
-                        margin: const EdgeInsets.only(top: 50.0),
-                        color: const Color.fromARGB(255, 225, 19, 157),
-                      ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 60.0),
+        
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TabBar(
+                    tabs: [
+                      Tab(text: "Time"),
+                      Tab(text: "Stopwatch"),
+                      Tab(text: "Timer"),
                     ],
                   ),
-                ),
-              ],
+                  Container(
+                    height: 250,
+                    width: 250,
+                    margin: const EdgeInsets.only(top: 50.0),
+                    color: const Color.fromARGB(255, 225, 19, 157),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
